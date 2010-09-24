2a6610ca1c2c9d10596a2592685f342dcccf53d1
centralniak <p.kilczuk@stylesheet.pl>
2010-09-24 15:57:21 +0200

    First version of project

---
+++ admin_langswitch/__init__.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.core import urlresolvers
+from django.core.exceptions import ImproperlyConfigured
+import os.path
+
+URL = ''
+
+# check if 'django.views.i18n.set_language' is reachable via url, otherwise raise ImproperlyConfigured
+try:
+    URL = urlresolvers.reverse('django.views.i18n.set_language')
+except urlresolvers.NoReverseMatch:
+    raise ImproperlyConfigured('Cannot resolve django.views.i18n.set_language to URL - see http://docs.djangoproject.com/en/1.2/topics/i18n/internationalization/#the-set-language-redirect-view')
+
+# register additional template dir
+HERE = os.path.abspath(os.path.dirname(__file__))
+settings.TEMPLATE_DIRS += (os.path.join(HERE, 'templates'),)
+
+# @todo: Add ctx processor which adds Template object to the context
+#        This way it will be possible for admin/base.html to extend admin/base.html from 
+#        django.contrib.admin
+#
+#        Another approach may be to write custom extension tag, which would make it possible
+#        to specify which exact app to use for finding parent tpl. Ex.:
+#        {% extendsfrom 'temp/late.html' 'django.contrib.admin' %}
