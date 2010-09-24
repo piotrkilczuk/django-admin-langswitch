15163c56309ca39573ec16abe7a2b11d3dc00452
centralniak <p.kilczuk@stylesheet.pl>
2010-09-24 16:02:46 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='admin_langswitch',
+    version='0.1',
+    author='Piotr Kilczuk',
+    author_email='p.kilczuk@neumea.pl',
+    url='http://github.com/centralniak',
+    description = 'Adds easy language switch in admin',
+    packages = ['admin_langswitch',],
+    package_data={'cmsplugin': ['templates/*',]},
+)
