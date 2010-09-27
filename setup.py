0b78e4814f8ec412ca8a46e2b428a4071bdc876b
centralniak <p.kilczuk@stylesheet.pl>
2010-09-27 11:54:10 +0200

    Incremented version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='admin_langswitch',
-    version='0.1',
+    version='0.1.1',
     author='Piotr Kilczuk',
     author_email='p.kilczuk@neumea.pl',
     url='http://github.com/centralniak',
